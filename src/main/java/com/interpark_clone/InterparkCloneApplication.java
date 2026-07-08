601b8cec0befb2a84d6ad3cde260f064ac9c499d
Hoyoung027 <hybyun0207@gmail.com>
2026-07-08 16:31:54 +0900

    init : spring setting

---
+++ src/main/java/com/interpark_clone/InterparkCloneApplication.java
@@ -0,0 +1,13 @@
+package com.interpark_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterparkCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterparkCloneApplication.class, args);
+	}
+
+}
